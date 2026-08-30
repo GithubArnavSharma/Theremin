3a2a19700bbd6949ea058ff9ac5c18219f44622f
Arnav Sharma <77365987+GithubArnavSharma@users.noreply.github.com>
2026-08-29 22:39:45 -0700

    Update and rename model_setup.hpp to model/model_setup.hpp

--- model/model_setup.hpp
+++ model/model_setup.hpp
@@ -7,6 +7,8 @@
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 
+#include "mic_setup.hpp"
+
 #define SAMPLE_RATE 16000
 #define BUFFER_SIZE 1024
 #define OUTPUT_SIZE 360
@@ -21,3 +23,5 @@ void setupModel();
 void inputModel(int16_t* StreamBuffer);
 
 bool outputModel();
+
+void modelTask(void* param);
