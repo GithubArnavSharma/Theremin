59e82e3672f5adc1bb578819cd16233c4d727dfe
Arnav Sharma <77365987+GithubArnavSharma@users.noreply.github.com>
2026-08-29 22:44:56 -0700

    Update PitchDetector.ino

--- PitchDetector.ino
+++ PitchDetector.ino
@@ -1,9 +1,3 @@
-//Take notes on: Partition Scheme(3 MB app?), quantization, 
-//hardware interuppts, diodes, BJT transistors(like pins),
-//uint8_t vs int8_t vs int32_t vs float vs double, volatile int, #define 
-//ArduTFLite and how all of the commands work
-//Tolerance for resistors?
-
 #include "mic_setup.hpp" 
 #include "model_setup.hpp"
 #include "quant.hpp" 
